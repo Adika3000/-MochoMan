485fab95e6f73d23520e162d2aa2a057e77601a4
Adika3000 <93367172+Adika3000@users.noreply.github.com>
2021-11-19 14:56:19 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="https://cdn5.vectorstock.com/i/1000x1000/51/99/icon-of-user-avatar-for-web-site-or-mobile-app-vector-3125199.jpg">
+    <link href='http://fonts.googleapis.com/css?family=Philosopher&subset=cyrillic' rel='stylesheet' type='text/css'>
+    
+      <title>Adil Adylbekov</title>
+      
+  </head>
+<html>
+<body>
+</head>
+<body>
+  <head>
+    <center>
+    <p>Chunchunmaru</p>
+    <p>Hi my name is Adil</p>
+    <div>
+    </div>
+  </center>
+
+  <html>
+  <div>
+    <a
+      <a href="https://store.steampowered.com/" target="_blank">Steam</a> |
+      <a href="https://vk.com/themoongang"target="_blank">Discord</a> |
+      <a href=""target="_blank">Vk</a>
+    </div>
+   
+    </div>
+    </head>
+</body>
+</html>
