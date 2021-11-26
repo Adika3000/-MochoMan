77b036f77b2e1aef343e37684dc843ef257464b6
Adika3000 <93367172+Adika3000@users.noreply.github.com>
2021-11-26 11:19:12 +0600

    Create adil.py

---
+++ adil.py
@@ -0,0 +1,11 @@
+<style type ="text/css">
+      form{
+          position: absolute;
+          margin-left: 35%;
+          margin-top: 20%;  
+        
+      }
+
+  .voiti{
+      margin-left: 43%;
+  }
